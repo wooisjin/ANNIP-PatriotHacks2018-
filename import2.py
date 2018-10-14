460e6f219525e9143c4b55d6ca5e66b7d8205efb
wooisjin <43529385+wooisjin@users.noreply.github.com>
2018-10-14 09:45:37 -0400

    Update import2.py

--- import2.py
+++ import2.py
@@ -14,19 +14,16 @@ with open("C:/Users/Owner/PycharmProjects/PatriotHacks2018/Excelit/hackathon_els
 with open("excel_data10.pkl", "wb") as afile:
     pickle.dump(cc, afile)
 
-"""
+zz = []
 with open("C:/Users/Owner/PycharmProjects/PatriotHacks2018/Excelit/answerkeyinex.csv", newline='') as csvfile:
     spamreader = csv.reader(csvfile, quotechar='|')
     for row in spamreader:
         try:
             _row = [int(x) for x in row]
-            cc+= _row
+            zz+= _row
         except ValueError:
             pass
 
     with open("anskey.pkl","wb") as file:
-        pickle.dump(cc, file)
-f = open("answerkey.txt", "w")
-simplejson.dump(cc, f)
-f.close()
-"""
+        pickle.dump(zz, file)
+        
